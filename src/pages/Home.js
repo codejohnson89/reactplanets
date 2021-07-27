71134a817817e5fd58b808f3b64742ebcd0d5f97
Micah Johnson <mjohnson@teleflora.org>
2021-07-27 11:36:18 -0500

    fix warning to avoid error while deploying

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 
 const Home = () => {
