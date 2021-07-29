af557d08ac45bde85d9460171af64b1bc4832727
Micah Johnson <mjohnson@teleflora.org>
2021-07-29 10:09:59 -0500

    Mobile friendly adjustments

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,9 +8,16 @@ const Buttons = () => {
     const dispatch = useDispatch();
     return (
         <div className="viewButtons">
+            <div className="desktop-btn">
                 <Button onClick={() => dispatch(showOverview())}>01<span>overview</span></Button>
                 <Button onClick={() => dispatch(showInternal())}>02<span>internal structure</span></Button>
                 <Button onClick={() => dispatch(showSurface())}>03<span>surface geology</span></Button>
+            </div>
+            <div className="mobile-btn">
+                <div onClick={() => dispatch(showOverview())}>overview</div>
+                <div onClick={() => dispatch(showInternal())}>internal</div>
+                <div onClick={() => dispatch(showSurface())}>surface</div>
+            </div>
         </div>
     )
 }
